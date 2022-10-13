79e5adf644e7de4d1bb5a2e53ee2c99bac3b52fd
zxzxvcdd <zxzxvcdd@gmail.com>
2022-10-13 18:28:50 +0900

    123

--- app.js
+++ app.js
@@ -7,10 +7,37 @@
 
 
 
+    initialize();
+
+    const $loginSection = document.querySelector("#note > .loginSection");
+    if ($loginSection != null) {
+      eventLogin();
+    }
+  
+    // hover to show dropdown menu
+    eventDropDown();
+  
+    // add event listener for each iconsㅋ
+    eventListenerIcon("fa-address-card");
+    eventListenerIcon("fa-credit-card");
+    eventListenerIcon("fa-bell");
+    eventListenerIcon("fa-hand-point-up");
+    eventListenerIcon("fa-heart");
+    eventListenerIcon("fa-hospital");
+    eventListenerIcon("fa-square-check");
+  
+    ShowMeThePage();
+    ShowMeTheTable();
+    
+
+
+
+
 
     
 
 
 
-})
+})();
+
 
